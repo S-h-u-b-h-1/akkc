58d77feaed00a0af55dab6e0c028cebc7d164989
S-h-u-b-h-1 <shubhaangkataruka22012007@gmail.com>
2026-06-06 10:28:38 +0530

    done

--- backend/src/controllers/adminTaskController.js
+++ backend/src/controllers/adminTaskController.js
@@ -1,25 +1,20 @@
+import { API_MESSAGES, HTTP_STATUS } from '../constants/api.js';
+import {
+  createAdminTask,
+  deleteAdminTask,
+  getAdminStats,
+  getAdminTask,
+  listAdminTasks,
+  updateAdminTask
+} from '../services/taskManagementService.js';
+import { sendSuccess } from '../utils/apiResponse.js';
 import { AppError } from '../utils/appError.js';
-
-export const createEmployee = async (req, res) => {
-  if (!req.user) {
-    throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
-  }
-  const employee = await createAdminEmployee({
-    adminId: req.user.id,
-    payload: req.validated.body
-  });
-
-  return sendSuccess(res, {
-    statusCode: HTTP_STATUS.CREATED,
-    message: API_MESSAGES.EMPLOYEE_CREATED,
-    data: { employee }
-  });
-};
 
 export const createTask = async (req, res) => {
   if (!req.user) {
     throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
   }
+
   const task = await createAdminTask({
     adminId: req.user.id,
     payload: req.validated.body
@@ -36,6 +31,7 @@ export const listTasks = async (req, res) => {
   if (!req.user) {
     throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
   }
+
   const tasks = await listAdminTasks({
     adminId: req.user.id,
     filters: req.validated.query
@@ -51,6 +47,7 @@ export const getTask = async (req, res) => {
   if (!req.user) {
     throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
   }
+
   const task = await getAdminTask({
     adminId: req.user.id,
     taskId: req.validated.params.id
@@ -66,6 +63,7 @@ export const updateTask = async (req, res) => {
   if (!req.user) {
     throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
   }
+
   const task = await updateAdminTask({
     adminId: req.user.id,
     taskId: req.validated.params.id,
@@ -78,26 +76,11 @@ export const updateTask = async (req, res) => {
   });
 };
 
-export const updateEmployee = async (req, res) => {
-  if (!req.user) {
-    throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
-  }
-  const employee = await updateAdminEmployee({
-    adminId: req.user.id,
-    employeeId: req.validated.params.id,
-    payload: req.validated.body
-  });
-
-  return sendSuccess(res, {
-    message: API_MESSAGES.EMPLOYEE_UPDATED,
-    data: { employee }
-  });
-};
-
 export const deleteTask = async (req, res) => {
   if (!req.user) {
     throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
   }
+
   const task = await deleteAdminTask({
     adminId: req.user.id,
     taskId: req.validated.params.id
@@ -109,25 +92,11 @@ export const deleteTask = async (req, res) => {
   });
 };
 
-export const deleteEmployee = async (req, res) => {
-  if (!req.user) {
-    throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
-  }
-  const employee = await deleteAdminEmployee({
-    adminId: req.user.id,
-    employeeId: req.validated.params.id
-  });
-
-  return sendSuccess(res, {
-    message: API_MESSAGES.EMPLOYEE_DELETED,
-    data: { employee }
-  });
-};
-
 export const getStats = async (req, res) => {
   if (!req.user) {
     throw new AppError(API_MESSAGES.UNAUTHORIZED, HTTP_STATUS.UNAUTHORIZED);
   }
+
   const stats = await getAdminStats({
     adminId: req.user.id
   });
